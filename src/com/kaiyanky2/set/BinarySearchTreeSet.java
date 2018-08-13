1ed60d591f7cf710419b21f5adb1c1918a667a98
wangkaiyan <kaiyanky2@gmail.com>
2018-08-13 23:53:12 +0800

    二分搜索树实现集合

---
+++ src/com/kaiyanky2/set/BinarySearchTreeSet.java
@@ -0,0 +1,38 @@
+package com.kaiyanky2.set;
+
+import com.kaiyanky2.binarytree.BinarySearchTree;
+
+public class BinarySearchTreeSet<E extends Comparable<E>> implements Set<E> {
+
+
+    private BinarySearchTree<E> binarySearchTree;
+
+    public BinarySearchTreeSet() {
+        binarySearchTree = new BinarySearchTree<>();
+    }
+
+    @Override
+    public void add(E e) {
+        binarySearchTree.add(e);
+    }
+
+    @Override
+    public boolean contains(E e) {
+        return binarySearchTree.contains(e);
+    }
+
+    @Override
+    public void remove(E e) {
+        binarySearchTree.remove(e);
+    }
+
+    @Override
+    public int getSize() {
+        return binarySearchTree.getSize();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return binarySearchTree.isEmpty();
+    }
+}
